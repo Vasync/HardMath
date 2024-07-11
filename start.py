a7d2d6cd46c951d16b59ba2789e2890add6401d0
LSpace <tbao12901@gmail.com>
2024-07-11 21:31:35 +0700

    Create start.py

---
+++ start.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+
+chooseMin = input("Input min int for math: ");
+chooseMax = input("Input max int fpr math: ");
+
+quest = random.randint(chooseMin, chooseMax) * random.randint(chooseMin, chooseMax) / random.randint(chooseMin, chooseMax) + random.randint(chooseMin, chooseMax) + random.randint(chooseMin, chooseMax) - random.randint(chooseMin, chooseMax)
+
+print("MATH: ", quest)
+
+
+if isinstance(, int) :
+  
